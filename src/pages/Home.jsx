901657b9410cf25517a45930f3ef3c17654ae1ac
FeuMangue <coelhohugo2003@gmail.com>
2024-04-22 00:23:42 +0200

    accueil presque fini

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
+
 const Home = () => {
-    return (
-        <div>
-            {/* Premier bloc */}
+  return (
+    <div>
+      {/* Premier bloc */}
       <div className="first-block">
         <div className="image-container">
           <img src="./hero-bg.jpg" alt="Image d'accueil" className="full-width" />
           <div className="centered">
-            <h1>Titre principal</h1>
-            <h2>Sous-titre</h2>
+            <h1 className="h1grand">Bonjour, je suis John Doe</h1>
+            <h2 className="h2grand">Développeur web full stack</h2>
+            <button className="btn btn-primary">En savoir plus</button>
           </div>
         </div>
       </div>
@@ -15,12 +18,28 @@ const Home = () => {
       {/* Deuxième bloc */}
       <div className="second-block">
         <div className="container">
-          <h1>À propos</h1>
-          {/* Ajoutez ici le contenu de votre deuxième bloc */}
+          <div className="row">
+            <div className="col-md-6">
+              {/* Partie de gauche */}
+              <div>
+                <h2 className='h2noir'>À propos</h2>
+                <hr className='trait-bleu' /> {/* Trait bleu */}
+                <p className='textenoir'>Texte de la partie gauche</p>
+              </div>
+            </div>
+            <div className="col-md-6">
+              {/* Partie de droite */}
+              <div>
+                <img src="./john-doe-about.jpg" alt="Image" />
+                <h2 className='h2noir'>Mes compétences</h2>
+                <p className='textenoir'>Texte de la partie droite</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default Home;
